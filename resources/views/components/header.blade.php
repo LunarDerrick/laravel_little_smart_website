a06a1fd8ee2967105d302f8c72a8349f526357b3
derrick phang <derrickphb@gmail.com>
2024-08-29 17:39:40 +0800

    29 August 2024: -minor fix on mobile view appear zoomed in initially. -WIP: fix/improve current modules -next: translate option

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="{{ mix('style.css') }}">
